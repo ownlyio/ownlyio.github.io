1cbeddf347ce3e600486900032c3dfb341152fb4
Lawrence <146691376+cedik456@users.noreply.github.com>
2024-01-17 07:05:20 +0800

    Add scroll padding

--- website/script.js
+++ website/script.js
@@ -1,13 +1,3 @@
-/* const primaryHeader = document.querySelector(".header");
-const navToggle = document.querySelector(".mobile-nav-toggle");
-const primaryNav = document.querySelector(".navigation");
+const navigationHeight = document.querySelector('header').offsetHeight
 
-navToggle.addEventListener('click', () => {
-  primaryNav.hasAttribute("data-visible") 
-  ? navToggle.setAttribute("aria-expanded", false)
-  : navToggle.setAttribute("aria-expanded", true)
-  primaryNav.toggleAttribute("data-visible");
-  primaryHeader.toggleAttribute("data-overlay");
-});
-
- */
+document.documentElement.style.setProperty('--scroll-padding', navigationHeight + "px");

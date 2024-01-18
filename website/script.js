8c1940fe1ccae79b67233106f31f3de2a3f04169
Lawrence <146691376+cedik456@users.noreply.github.com>
2024-01-18 18:16:33 +0800

    Fix navigation

--- website/script.js
+++ website/script.js
@@ -1,3 +1,15 @@
-const navigationHeight = document.querySelector('header').offsetHeight
+/*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) =>{
+  const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
 
-document.documentElement.style.setProperty('--scroll-padding', navigationHeight + "px");
+  toggle.addEventListener('click', () =>{
+      // Add show-menu class to nav menu
+      nav.classList.toggle('show-menu')
+
+      // Add show-icon to show and hide the menu icon
+      toggle.classList.toggle('show-icon')
+  })
+}
+
+showMenu('nav-toggle','nav-menu')

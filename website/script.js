238bc4da5ce39150671109a54cf9a820560bab6a
Lawrence <146691376+cedik456@users.noreply.github.com>
2024-01-21 08:36:21 +0800

    Add dark mode feature

--- website/script.js
+++ website/script.js
@@ -1,4 +1,4 @@
-/*=============== SHOW MENU ===============*/
+/* SHOW MENU */
 const showMenu = (toggleId, navId) =>{
   const toggle = document.getElementById(toggleId),
         nav = document.getElementById(navId)
@@ -13,3 +13,14 @@ const showMenu = (toggleId, navId) =>{
 }
 
 showMenu('nav-toggle','nav-menu')
+
+/* DARK MODE */
+
+const body = document.querySelector("body"),
+      nav = document.querySelector("nav"),
+      modeToggle = document.querySelector(".dark__mode");
+
+      modeToggle.addEventListener("click", () =>{
+        modeToggle.classList.toggle("active");
+        body.classList.toggle("dark");
+      })
